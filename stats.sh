517902f277a901c6014b4cc32ecb993782797a9c
konsalex <konsalexee@gmail.com>
2022-05-02 12:28:31 +0200

    shellcheck review

--- stats.sh
+++ stats.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Count of all "imports" and "exports" in projects (exports is used in Aura)
 rg -c --pcre2 "^(import|export).+(\"|\')@neo4j-ndl\/react(\"|\')" | wc -l
 
@@ -7,19 +9,18 @@ rg -c --pcre2 "^(import|export).+(\"|\')@neo4j-ndl\/react(\"|\')" | wc -l
 # get the first dir of the path (repo name) >
 # count the unique dirs
 all_repos=$(rg --pcre2 "^(import|export).+(\"|\')@neo4j-ndl\/react(\"|\')" |
-awk 'NR % 3 == 0' |
-rg --only-matching --pcre2 "^[^\/]*" |
-sort |
-uniq)
+    awk 'NR % 3 == 0' |
+    rg --only-matching --pcre2 "^[^\/]*" |
+    sort |
+    uniq)
 
-echo $all_repos
+echo "$all_repos"
 
-all_repos_count=$(echo $all_repos | wc -w)
+all_repos_count=$(echo "$all_repos" | wc -w)
 
-echo $all_repos_count
+echo "$all_repos_count"
 
 # Total repos tracked
 total_repos=$(ls -d -- */ | wc -l)
 
 echo "${all_repos_count} out of ${total_repos} tracked"
-
